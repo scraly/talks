ce839465b4cf5864d692f2b0980b322edb770644
Vache, Aurelie <aurelie.vache@continental-corporation.com>
2019-06-09 18:31:45 +0200

    init tf

---
+++ terraform/variables.tf
@@ -0,0 +1,11 @@
+variable "aws_s3_bucket_terraform" {
+    default = "com.scraly.terraform"
+}
+
+variable "aws_s3_bucket_cloudfront" {
+    default = "com.scraly.cloudfront"
+}
+
+variable "aws_s3_bucket_front" {
+    default = "com.scraly.front"
+}
